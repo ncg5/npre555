7ab38c5e08b2298279b9bf005428cb7004a8d890
Natalie Gaughan <ncg5@illinois.edu>
2020-10-05 13:54:38 -0500

    added setup file and modified workflow to call it

---
+++ CP1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 13:24:19 2020
+
+@author: natal
+"""
+
+#NPRE 555 CP 1
+#This file contains the necessary information to setup the problem, like geometry and material definitions
+
+histories=10
+
+#geometry
+slab=100 #slab thickness in cm
+mid=50 #location of region boundary in cm
+
+#material properties
+nu=2.4
+#region 1 (left side, 0<x<mid)
+Sigma_a1=0.12 #absorption (cm-1) 
+Sigma_s1=0.05 #scattering
+Sigma_f1=0.15/nu #fission
+Sigma_c1=Sigma_a1-Sigma_f1 #capture 
+Sigma_t1=Sigma_a1+Sigma_s1 #total
+#region 2 (right side, mid<x<slab)
+Sigma_a2=0.10 #cm-1
+Sigma_s2=0.05
+Sigma_f2=0.12/nu
+Sigma_c2=Sigma_a2-Sigma_f2 
+Sigma_t2=Sigma_a2+Sigma_s2
+
+Sigma_a=[Sigma_a1,Sigma_a2]
+Sigma_c=[Sigma_c1,Sigma_c2]
+Sigma_f=[Sigma_f1,Sigma_f2]
+Sigma_s=[Sigma_s1,Sigma_s2]
+Sigma_t=[Sigma_t1,Sigma_t2]
--- CP1/workflow.py
+++ CP1/workflow.py
@@ -10,53 +10,48 @@ Created on Thu Oct  1 22:00:02 2020
 
 import numpy as np
 
-#geometry
-slab=100 #slab thickness in cm
-mid=50 #location of region boundary in cm
-
-#material properties
-#region 1 (left side, 0<x<mid)
-nu=2.4
-Sigma_a1=0.12 #cm-1
-Sigma_s1=0.05
-Sigma_f1=0.15/nu
-Sigma_t1=Sigma_a1+Sigma_s1
-#region 2 (right side, mid<x<slab)
-Sigma_a2=0.10 #cm-1
-Sigma_s2=0.05
-Sigma_f2=0.12/nu
-Sigma_t2=Sigma_a2+Sigma_s2
+#histories=10 #number of histories/cycles to run
+from setup import histories, slab, mid, Sigma_a, Sigma_c, Sigma_f, Sigma_s, Sigma_t  
 
 
-#STEP 1: new particle: sample position, energy, and angle
-pos=slab*np.random.rand() #position (x-coordinate) in cm
-
-#sample energy from chi distribution - Do I need this?
-#sample angle isotropically - Do I need this?
-ang=2*np.pi*np.random.rand()
-mu=np.cos(ang)
-
-
-#STEP 2: determine distance to regional boundary
-#distance to left boundary (x=0) = pos
-dist_mid=np.absolute(pos-mid) #distance to region boundary in cm
-dist_R=slab-pos #distance to right boundary (x=slab) in cm
-distances=[pos,dist_mid,dist_R]
-closest_bound=np.argmin(distances)
-dist_bound=distances[closest_bound]
-
-
-#STEP 3: determine distance to next collision
-xi_col=np.random.rand() #generate a random number xi
-if pos<mid: #region 1
-    dist_col=-np.log(1-xi_col)/Sigma_t1
-else: #region 2
-    dist_col=-np.log(1-xi_col)/Sigma_t2
+for n in range(histories):
+    #STEP 1: new particle: sample position, energy, and angle
+    pos=slab*np.random.rand() #position (x-coordinate) in cm
+    if pos<mid:
+        reg=0 #will use this to index cross sections
+    else:
+        reg=1
     
-
-#STEP 4: determine if a collison or boundary crossing happens first
-event_dist=[dist_bound, dist_col] 
-next_event=np.argmin(event_dist) #0=boundary crossing, 1=collision
-track_length=event_dist[next_event] #store the distance to collision
-
-
+    #sample energy from chi distribution - Do I need this?
+    #sample angle isotropically - Do I need this?
+    ang=2*np.pi*np.random.rand()
+    mu=np.cos(ang)
+    
+    
+    #STEP 2: determine distance to regional boundary
+    #distance to left boundary (x=0) = pos
+    dist_mid=np.absolute(pos-mid) #distance to region boundary in cm
+    #maybe dont use absval here, but instead use it in "distances" so I can get new pos later
+    dist_R=slab-pos #distance to right boundary (x=slab) in cm
+    distances=[pos,dist_mid,dist_R]
+    closest_bound=np.argmin(distances)
+    dist_bound=distances[closest_bound]
+    
+    
+    #STEP 3: determine distance to next collision
+    xi_col=np.random.rand() #generate a random number xi
+    # if pos<mid: #region 1
+    #     dist_col=-np.log(1-xi_col)/Sigma_t1
+    # else: #region 2
+    #     dist_col=-np.log(1-xi_col)/Sigma_t2
+    dist_col=-np.log(1-xi_col)/Sigma_t[reg]    
+    
+    #STEP 4: determine if a collison or boundary crossing happens first
+    event_dist=[dist_bound, dist_col] 
+    event_type=np.argmin(event_dist) #0=boundary crossing, 1=collision
+    track_length=event_dist[event_type] #store the distance to collision
+    #if event_type=0, pos gets adjusted then repeat from step 2
+    
+    # if event_type==1: #for collisions, determine the collision type
+        #xi_coltype=np.random.rand() #generate a random number xi
+        #make a function to determine collision type
