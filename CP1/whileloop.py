81f8f5eb4b2bbd81285506972acd69be3d13d407
Natalie Gaughan <ncg5@illinois.edu>
2020-10-06 16:53:03 -0500

    implemented sampling new direction after scatter

--- CP1/whileloop.py
+++ CP1/whileloop.py
@@ -13,12 +13,13 @@ for n in range(histories):
     #sample position and direction
     pos=np.random.rand()
     direc=np.random.randint(0,2) #0=going left, 1=going right
-    
+    #Because this problem is 1D, the only sense of an angle that we need is whether the neutron is moving right or left
+
     active=True #use this to tell the code when to end a history
 
     while active==True:
         if pos<mid:
-            reg=0 #which region: 0=left, 1=right. Will use this to index cross sections
+            reg=0 #which region the neutron is in: 0=left, 1=right. Will use this to index cross sections
         else:
             reg=1
         
@@ -37,7 +38,7 @@ for n in range(histories):
         
         #determine next event
         if dist_bound<dist_col: #crosses boundary
-            next_event=0 
+            # next_event=0 
             print('boundary crossing')
             if reg==0 and direc==0: #in left region and moving left - will cross L edge
                 active=False #neutron leaves slab by left edge
@@ -53,7 +54,7 @@ for n in range(histories):
                 print('leaves by right')
 
         else:   #collision
-            next_event=1 
+            # next_event=1 
             print('collision')
             #determine type of collision
             det_col=np.random.randint(0,3)
@@ -66,5 +67,7 @@ for n in range(histories):
                 active=False
             else: 
                 print('scatter')
+                #determine new direction: assume isotropic, so both directions are equally likely
+                direc=np.random.randint(0,2) #0=going left, 1=going right
         
         print('do the loop again')
